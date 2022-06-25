6f49a43d31d71374a5b1017674828d2eae63449d
joaolmb <joaolmb2@gmail.com>
2022-06-24 21:52:53 -0300

    Iniciando com React no blog pessoal

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+    return(
+        <>
+        <h1 className="titulo">Home</h1>
+        <img src="https://images.pexels.com/photos/2253275/pexels-photo-2253275.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Imagem de um Cachorro" className="img" />
+        </>
+    );
+}
+
+export default Home;
