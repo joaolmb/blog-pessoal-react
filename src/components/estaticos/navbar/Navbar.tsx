52a84d416afdc5654c063b68d738c3935209dabb
joaolmb <joaolmb2@gmail.com>
2022-06-27 20:24:21 -0300

    Adicionando navbar, home e footer

---
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -0,0 +1,48 @@
+import { Toolbar, Box } from '@mui/material';
+import { Typography, AppBar } from '@material-ui/core';
+
+function Navbar() {
+    return (
+        <>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                        <Box style={{ cursor: "pointer" }} >
+                            <Typography variant="h5" color="inherit">
+                                Blog do Bento
+                            </Typography>
+                        </Box>
+
+                        <Box display="flex" justifyContent="start">
+                            <Box mx={1} style={{ cursor: "pointer" }}>
+                                <Typography variant="h6" color="inherit">
+                                    Home
+                                </Typography>
+                            </Box>
+                            <Box mx={1} style={{ cursor: "pointer" }}>
+                                <Typography variant="h6" color="inherit">
+                                    Posts
+                                </Typography>
+                            </Box>
+                            <Box mx={1} style={{ cursor: "pointer" }}>
+                                <Typography variant="h6" color="inherit">
+                                    Temas
+                                </Typography>
+                            </Box>
+                            <Box mx={1} style={{ cursor: "pointer" }}>
+                                <Typography variant="h6" color="inherit">
+                                    Cadastrar tema
+                                </Typography>
+                            </Box>
+                            <Box mx={1} style={{ cursor: "pointer" }}>
+                                <Typography variant="h6" color="inherit">
+                                    Logout
+                                </Typography>
+                            </Box>
+                        </Box>
+                    </Toolbar>
+                </AppBar>
+        </>
+    );
+}
+
+export default Navbar;
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,11 +1,40 @@
-import React from "react";
 import "./Home.css";
+import { Box, Grid, Typography } from '@mui/material'
+import { Paper, Button } from '@material-ui/core'
 
 function Home() {
-    return(
+    return (
         <>
-        <h1 className="titulo">Home</h1>
-        <img src="https://images.pexels.com/photos/2253275/pexels-photo-2253275.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Imagem de um Cachorro" className="img" />
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
+            <Paper>
+                <Box p={2}>
+                    <Box display="flex" justifyContent="center">
+                        <h1>Afinal, o Hulk de terno é o:</h1>
+                    </Box>
+                    <img src="https://pbs.twimg.com/media/ET-2E_dXkAIonYV.png" alt="Hulk Agiota" style={{ width: "100%", height: "100%" }} />
+                    <Box display="flex" justifyContent="center" p={2}>
+                        <Button variant="contained" color="primary">Hulk Agiota</Button>
+                        <Button variant="contained" color="secondary">Hulk Cinéfilo</Button>
+                    </Box>
+                </Box>
+            </Paper>
         </>
     );
 }
