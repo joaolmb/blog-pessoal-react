bed4e9d614f42738f3adc6f4f087cd78d958500a
joaolmb <joaolmb2@gmail.com>
2022-06-29 13:50:18 -0300

    criando login e refatorando código para que ele fique mais organizado

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,46 +1,50 @@
-import { Toolbar, Box } from '@mui/material';
-import { Typography, AppBar } from '@material-ui/core';
+import { Box } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { Typography, AppBar, Toolbar } from '@material-ui/core';
+import './Navbar.css'
 
 function Navbar() {
     return (
         <>
-            <AppBar position="static">
-                <Toolbar variant="dense">
-                        <Box style={{ cursor: "pointer" }} >
-                            <Typography variant="h5" color="inherit">
-                                Blog do Bento
+            <AppBar position="static" className='appbar'>
+                <Toolbar variant="dense" className='toolbar'>
+                    <Box className='cursor' >
+                        <Typography variant="h5" color="inherit" className='fonte'>
+                            Blog do Bento
+                        </Typography>
+                    </Box>
+
+                    <Box display="flex" justifyContent="start" >
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                Home
                             </Typography>
                         </Box>
-
-                        <Box display="flex" justifyContent="start">
-                            <Box mx={1} style={{ cursor: "pointer" }}>
-                                <Typography variant="h6" color="inherit">
-                                    Home
-                                </Typography>
-                            </Box>
-                            <Box mx={1} style={{ cursor: "pointer" }}>
-                                <Typography variant="h6" color="inherit">
-                                    Posts
-                                </Typography>
-                            </Box>
-                            <Box mx={1} style={{ cursor: "pointer" }}>
-                                <Typography variant="h6" color="inherit">
-                                    Temas
-                                </Typography>
-                            </Box>
-                            <Box mx={1} style={{ cursor: "pointer" }}>
-                                <Typography variant="h6" color="inherit">
-                                    Cadastrar tema
-                                </Typography>
-                            </Box>
-                            <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                Posts
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                Temas
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                Cadastrar tema
+                            </Typography>
+                        </Box>
+                        <Link to='/login' className='text-decoration-none'>
+                            <Box mx={1} className='cursor'>
                                 <Typography variant="h6" color="inherit">
                                     Logout
                                 </Typography>
                             </Box>
-                        </Box>
-                    </Toolbar>
-                </AppBar>
+                        </Link>
+                    </Box>
+                </Toolbar>
+            </AppBar>
         </>
     );
 }
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,40 +1,28 @@
 import "./Home.css";
-import { Box, Grid, Typography } from '@mui/material'
+import { Box, Grid, Typography, TextField } from '@mui/material'
 import { Paper, Button } from '@material-ui/core'
 
 function Home() {
     return (
         <>
-            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className="caixa">
                 <Grid alignItems="center" item xs={6}>
                     <Box paddingX={20} >
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className="titulo">Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className="titulo">expresse aqui os seus pensamentos e opiniões!</Typography>
                     </Box>
                     <Box display="flex" justifyContent="center">
                         <Box marginRight={1}>
                         </Box>
-                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                        <Button variant="outlined" className="botao">Ver Postagens</Button>
                     </Box>
                 </Grid>
                 <Grid item xs={6} >
                     <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
                 </Grid>
-                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                <Grid xs={12} >
                 </Grid>
             </Grid>
-            <Paper>
-                <Box p={2}>
-                    <Box display="flex" justifyContent="center">
-                        <h1>Afinal, o Hulk de terno é o:</h1>
-                    </Box>
-                    <img src="https://pbs.twimg.com/media/ET-2E_dXkAIonYV.png" alt="Hulk Agiota" style={{ width: "100%", height: "100%" }} />
-                    <Box display="flex" justifyContent="center" p={2}>
-                        <Button variant="contained" color="primary">Hulk Agiota</Button>
-                        <Button variant="contained" color="secondary">Hulk Cinéfilo</Button>
-                    </Box>
-                </Box>
-            </Paper>
         </>
     );
 }
