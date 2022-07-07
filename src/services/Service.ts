47f632d52b19c8e90b07c83a5439fee788f6cd5f
joaolmb <joaolmb2@gmail.com>
2022-07-07 14:32:04 -0300

    oi oi oi joão vitor

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -4,18 +4,37 @@ export const api = axios.create({
     baseURL: 'https://blogpessoaljoaol.herokuapp.com'
 })
 
-export const cadastroUsuario = async(url: any, dados: any, setDado: any) => {
+export const cadastroUsuario = async (url: any, dados: any, setDado: any) => {
     const resposta = await api.post(url, dados)
     setDado(resposta.data)
 }
 
 // como se fosse um /usuarios/logar
-export const login = async(url: any, dados: any, setDado: any) => {
+export const login = async (url: any, dados: any, setDado: any) => {
     const resposta = await api.post(url, dados)
     setDado(resposta.data.token)
 }
 
-export const buscar = async (url: any, setDado:any, header: any) => {
+export const buscar = async (url: any, setDado: any, header: any) => {
     const resposta = await api.get(url, header)
     setDado(resposta.data)
+}
+
+export const buscarId = async (url: any, setDado: any, header: any) => {
+    const resposta = await api.get(url, header)
+    setDado(resposta.data)
+}
+
+export const post = async (url: any, dados: any, setDado: any, header: any) => {
+    const resposta = await api.post(url, dados, header)
+    setDado(resposta.data)
+}
+
+export const put = async (url: any, dados: any, setDado: any, header: any) => {
+    const resposta = await api.put(url, dados, header)
+    setDado(resposta.data)
+}
+
+export const deleteId = async (url: any, header: any) => {
+    await api.delete(url, header)
 }
